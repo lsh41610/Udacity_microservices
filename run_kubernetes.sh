632c5954a046913529fb7e6274c4152f0022819c
Ian <lsh41610@gmail.com>
2021-11-14 02:27:41 +0900

    create kubernetes_out.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="lastgeneral/udacity_microservices"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitymicroservices --image=$dockerpath --port=80 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacitymicroservices 8000:80
